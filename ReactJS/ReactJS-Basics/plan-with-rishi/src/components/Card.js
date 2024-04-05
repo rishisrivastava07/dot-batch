943ded48f5533e025df38d8271db7b94d973fb31
Rishi Srivastava <rishisrivastava.ju@gmail.com>
2024-04-05 12:02:24 +0530

    Some changes made in all projects

--- ReactJS/ReactJS-Basics/plan-with-rishi/src/components/Card.js
+++ ReactJS/ReactJS-Basics/plan-with-rishi/src/components/Card.js
@@ -15,7 +15,7 @@ function Card({ id, image, info, price, name, removeTour}) {
       <img src={image} alt="" className="image" />
       <div className="tour-info">
         <div className="tour-details">
-            <h4 className="tour-price">&#8377; ${price}</h4>
+            <h4 className="tour-price">&#8377; {price}</h4>
             <h4 className="tour-name">{name}</h4>
         </div>
         <div className="tour-description">
