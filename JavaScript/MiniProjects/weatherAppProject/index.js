c203f760d944972c8f7dcdce9a36df87c6fe922f
Rishi Srivastava <rishisrivastava.ju@gmail.com>
2024-04-14 10:21:27 +0530

    Weather App completed

--- JavaScript/MiniProjects/weatherAppProject/index.js
+++ JavaScript/MiniProjects/weatherAppProject/index.js
@@ -6,6 +6,7 @@ const grantAccessContainer = document.querySelector(".grant-location-container")
 const searchForm = document.querySelector("[data-searchForm]");
 const loadingScreen = document.querySelector(".loading-container");
 const userInfoContainer = document.querySelector(".user-info-container");
+const errorContainer = document.querySelector(".error-container");
 
 //initially vairables need????
 
@@ -19,6 +20,8 @@ function switchTab(newTab) {
         oldTab.classList.remove("current-tab");
         oldTab = newTab;
         oldTab.classList.add("current-tab");
+
+        errorContainer.style.display = 'none';
 
         if(!searchForm.classList.contains("active")) {
             //kya search form wala container is invisible, if yes then make it visible
@@ -104,10 +107,10 @@ function renderWeatherInfo(weatherInfo) {
     countryIcon.src = `https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png`;
     desc.innerText = weatherInfo?.weather?.[0]?.description;
     weatherIcon.src = `http://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png`;
-    temp.innerText = weatherInfo?.main?.temp;
-    windspeed.innertext = weatherInfo?.wind?.speed;
-    humidity.innertext = weatherInfo?.main?.humidity;
-    cloudiness.innerText = weatherInfo?.clouds?.all;
+    temp.innerText = `${weatherInfo?.main?.temp} °C`;
+    windspeed.innerText = `${weatherInfo?.wind?.speed} m/s`;
+    humidity.innerText = `${weatherInfo?.main?.humidity} %`;
+    cloudiness.innerText = `${weatherInfo?.clouds?.all} %`;
 
 
 }
@@ -158,13 +161,29 @@ async function fetchSearchWeatherInfo(city) {
         const response = await fetch(
             `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
           );
-        const data = await response.json();
-        loadingScreen.classList.remove("active");
-        userInfoContainer.classList.add("active");
-        renderWeatherInfo(data);
+        if(response.ok){
+            const data = await response.json();
+            loadingScreen.classList.remove("active");
+            userInfoContainer.classList.add("active");
+            errorContainer.style.display = 'none';
+            renderWeatherInfo(data);
+        }
+        else {
+            throw new Error('City not found');
+        }
     }
     catch(err) {
-        //hW
-        alert('City not found');
+        loadingScreen.classList.remove("active");
+        showError();
     }
 }
+
+// Error handling
+function showError() {
+    errorContainer.style.display = 'flex';
+    userInfoContainer.classList.remove('active');
+    grantAccessContainer.classList.remove('active');
+    loadingScreen.classList.remove('active');
+    searchForm.classList.add('active');
+}
+
